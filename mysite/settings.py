b9f8db46c47c27b499d5a586c2f42716fb393fda
David <david.daso@sterling.ng>
2023-04-11 12:30:06 +0100

    validate settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,8 @@ env = environ.Env(DEBUG=(bool, False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
